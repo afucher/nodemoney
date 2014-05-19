c50a4b38a01a4e67a85e70478391bae0a6e79207
Arthur Fücher <arthurfucher@gmail.com>
2014-05-19 14:53:04 -0300

    removed profile.ejs fail render

--- app/routes.js
+++ app/routes.js
@@ -45,9 +45,7 @@ module.exports = function(app, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
+		res.render('accounts.ejs');
 	});
 
 		// process the signup form
